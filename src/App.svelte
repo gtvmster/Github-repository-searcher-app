f1c10ef9469173f4a76bf693c87ebed7da81248b
gtvmster <89914851+gtvmster@users.noreply.github.com>
2021-12-29 15:15:59 -0300

    commit com novas alterações, o buscar de repositórios funciona, agora vamos para a parte visual e colocar a barra e lista do Svelte MUI.

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Request from './components/Request.svelte';
+	import Fixed from './components/Fixed.svelte'
+</script>
+
+
+<link rel="stylesheet" href="node_modules/@smui/top-app-bar/bare.css">
+
+<Request/>
+	  
+
+<style type="text/css">
+
+</style>
